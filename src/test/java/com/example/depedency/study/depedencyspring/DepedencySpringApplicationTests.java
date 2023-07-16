985cbb3ed9424e1b3ac2288dfa865543da2d5a3b
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-07-16 12:06:04 -0300

    feat:initial repo

---
+++ src/test/java/com/example/depedency/study/depedencyspring/DepedencySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.depedency.study.depedencyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepedencySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
