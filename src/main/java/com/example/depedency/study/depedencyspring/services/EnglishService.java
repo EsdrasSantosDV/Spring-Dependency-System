e39064dfaa50b3c91c6ca12b2d02b694e3e53d21
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-07-16 13:52:30 -0300

    feat:default profile

--- src/main/java/com/example/depedency/study/depedencyspring/services/EnglishService.java
+++ src/main/java/com/example/depedency/study/depedencyspring/services/EnglishService.java
@@ -3,7 +3,10 @@ package com.example.depedency.study.depedencyspring.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+//PODEMOS TER PERFIS, E DIZER QUAL SERVIÇO VAI SER UTILIZADO DEPENDENDO DO PERFIL X
+//PARA ISSO, BASTA COLOCAR O PROFILE NO SERVICE
+//O PROFILE DEFAULT, E O PROFILE QUE VAI SER UTILIZADO CASO NENHUM OUTRO PROFILE ESTEJA ATIVO
+@Profile({"EN","default"})
 @Service("i18NService")
 public class EnglishService implements HelloService{
     @Override
